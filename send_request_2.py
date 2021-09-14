8434c96b443056bd5e6841eff79ab436ad08021f
Максим Черняк _ Win_laptop <maximchernyak98@gmail.com>
2021-09-14 15:54:46 +0300

    Добавил запуск через pytest

--- send_request_2.py
+++ send_request_2.py
@@ -2,18 +2,16 @@ from config import uri, payload, headers
 import requests
 import json
 from helpers import debug_save_to_json_request_and_response
+import pytest
 
 
-def send_request_goods_item_search(uri, payload, headers):
+def test_send_request_goods_item_search(uri, payload, headers):
     url = uri + '/goodsItemSearch'
     payload['Body']["Id"] = 3079071
 
     response = requests.post(url, data=json.dumps(payload, indent=4), headers=headers)
-    debug_save_to_json_request_and_response(payload=payload, response=response, test_purpose='тест')
+    debug_save_to_json_request_and_response(payload=payload, response=response, test_purpose='тест2')
 
 
-def send_request_delivery_info(uri, payload, headers):
+def test_send_request_delivery_info():
     pass
-
-
-send_request_goodsItemSearch(uri, payload, headers)
